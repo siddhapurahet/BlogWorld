fa53ab3c852144d907c1ea19ce42c4760006ca96
Het Siddhapura <het@aarogram.com>
2024-02-26 16:50:58 +0530

    Deploy version-1

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+require('dotenv').config();
 const connectDatabase =async  () => {
 
     await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
